1cd641d10b2a0d5bc789779b8a42c271f007e3a5
Afsaneh Mollasalehi <salehi.afsane23@gmail.com>
2024-06-03 23:34:37 +0200

    adder in our addToChart feature

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -2,14 +2,15 @@ import { useContext } from "react"
 import { ProductsContext } from "../../contexts/products.context"
 import ProductCard from "../../components/product-card/product-card.component"
 import './shop.styles.scss'
-const Shop = () => {
 
+
+const Shop = () => {
   const {products}=useContext(ProductsContext)
+
   return (
           <div className="shop_container">
-            {products.map((product)=> (
-                <ProductCard  key={product.id} product={product}/>
-            ))}
+            {products.map((product)=> 
+                <ProductCard  key={product.id} product={product}/>)}
           </div>
           )
 }
